6f7eb07d908e92636fd9289fc38ef77c6251bd76
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2018-07-17 11:18:15 +0200

    Fix dependencies (should not be dev dependencies)

--- consumer.js
+++ consumer.js
@@ -1,14 +1,6 @@
 const AmqpService = require('.')
 const amqpService = new AmqpService()
 
-const messageHandler = (message) => {
-  console.log('--------- MESSAGE RECEIVED -----------')
-  console.log(message.payload)
-  console.log('--------------------------------------')
-  return message.release()
-}
-
-const queueName = 'test-queue'
 amqpService.listen('test-queue', (message) => {
   console.log('Message received:', message.payload)
   return message.release()
