e6885dca33d539343b96dc048513c0d05a15e568
Thibaut Gatouillat <thibaut.gatouillat@gmail.com>
2018-07-18 12:32:12 +0200

    Add examples

---
+++ examples/consumer.js
@@ -0,0 +1,14 @@
+const AmqpService = require('..')
+const amqpService = new AmqpService()
+
+amqpService.start('amqp://localhost')
+
+amqpService.listen('test-queue', (message) => {
+  console.log('Message received:', message.payload)
+  return message.release()
+})
+  .then(() => {
+    console.log('Listening for messages')
+  }).catch((error) => {
+    console.log(`Error while listening to queue ${queueName}:`, error)
+  })
